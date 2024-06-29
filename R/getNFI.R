13c2d7059a758183b191c4909a840edca229a4e3
wihe_EliteBook <wilson.lara@nibio.no>
2024-06-29 02:11:38 +0200

    New function: getNFI.R

--- R/getNFI.R
+++ R/getNFI.R
@@ -1,33 +1,37 @@
 getNFI <- structure(function#get NFI data
-### This function can download data sets of the Spanish National
-### Forest Inventory (SNFI) stored in the SNFI web page
-### (\code{"http://www.miteco.gob.es"}).
-                      ##details<< 
+### This function downloads data sets from the 2nd to the 4th stages of
+### the Spanish National Forest Inventory (SNFI) from the SNFI
+### website ("http://www.miteco.gob.es").
 (
-    prov,  ##<<\code{character} or \code{numeric}. Code of a Province
-           ##of SPain.
-    nfi = 4, ##<< \code{numeric}. Stage of the NFI. Default 4.
-    ... ##<< Arguments in \code{\link{readNFI}}.
-    
+    provincia,  ##<< Either a \code{character} or
+                ##\code{numeric}. Specifies the code of a Spanish
+                ##province.
+    nfi = 4, ##<< A \code{numeric} value. Indicates the stage of the
+             ##NFI. Default is set to 4.
+    ... ##<< Arguments used in \code{\link{readNFI}}.
 ) {
     if(is.numeric(nfi))
         nfi <- paste0('nfi',nfi)
-    url_ <- do.call(nfi, list(prov = prov))
+    url_ <- provincia
+    if(!grepl('.*\\..*', provincia))
+    url_ <- do.call(nfi, list(prov = provincia))
     ## return(url_)
     read <- readNFI(url_, ...)
-return(read)
-### \code{data.frame}. A data base  of the NFI.
-}, ex = function(){
-madridNFI <- system.file("ifn3p28_tcm30-293962.zip", package="basifoR")
-rmad <- readNFI(madridNFI)[1:100,]
-head(rmad)
+    return(read)
+### \code{data.frame}. This function returns a database of the NFI.
+}, ex = function(){madridNFI <- system.file("ifn3p28_tcm30-293962.zip", package="basifoR")
+    rmad <- readNFI(madridNFI)[1:100,]
+    head(rmad)
 
-## Downloading a data base from "http://www.miteco.gob.es" corresponding to the Spanish province of 'Madrid' established during the second stage of the NFI:
 
-## donttest{
-## rnfi <- getNFI(28, nfi = 2)[1:100,]
-## str(rnfi,3)
-## }
+    ## This will download a database from "http://www.miteco.gob.es"
+    ## corresponding to the Spanish province of 'Madrid' from the
+    ## second stage of the NFI:
+    
+    ## donttest{
+    ## rnfi <- getNFI(28, nfi = 2)[1:100,]
+    ## str(rnfi,3)
+    ## }
 
 
 })
